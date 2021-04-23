616a932fb3e3d97f4f837a1ac229618bceba5061
teradag <qohuck@gmail.com>
2021-04-24 00:51:09 +0900

    refactor(camerax): コードの書き方、改行位置などを調整する(動作影響なし)。

--- camerax/src/main/java/com/teradag/camerax/CameraActivity.kt
+++ camerax/src/main/java/com/teradag/camerax/CameraActivity.kt
@@ -35,7 +35,9 @@ class CameraActivity : AppCompatActivity() {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
             )
         }
 
@@ -51,16 +53,18 @@ class CameraActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
+            if (allPermissionsGranted().not()) {
                 Toast.makeText(
                     this,
                     "Permissions not granted by the user.",
                     Toast.LENGTH_SHORT
                 ).show()
+
                 finish()
+                return
             }
+
+            startCamera()
         }
     }
 
@@ -70,72 +74,81 @@ class CameraActivity : AppCompatActivity() {
         val photoFile = File(
             outputDirectory,
             SimpleDateFormat(
-                FILENAME_FORMAT, Locale.US
+                FILENAME_FORMAT,
+                Locale.US
             ).format(System.currentTimeMillis()) + ".jpg"
         )
 
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile)
+            .build()
 
         imageCapture.takePicture(
             outputOptions,
             ContextCompat.getMainExecutor(this),
             object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                override fun onError(e: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${e.message}", e)
                 }
 
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     val savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
+                    val succeededMessage = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, succeededMessage, Toast.LENGTH_SHORT)
+                        .show()
+                    Log.d(TAG, succeededMessage)
                 }
-            })
+            }
+        )
     }
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        cameraProviderFuture.addListener(Runnable {
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(view_finder.surfaceProvider)
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(view_finder.surfaceProvider)
+                    }
+                imageCapture = ImageCapture.Builder()
+                    .build()
+
+                try {
+                    cameraProvider.unbindAll()
+
+                    cameraProvider.bindToLifecycle(
+                        this,
+                        cameraSelector,
+                        preview,
+                        imageCapture
+                    )
+                } catch (e: Exception) {
+                    Log.e(TAG, "Use case binding failed", e)
                 }
 
-            imageCapture = ImageCapture.Builder()
-                .build()
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                cameraProvider.unbindAll()
-
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture
-                )
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
+            },
+            ContextCompat.getMainExecutor(this)
+        )
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it
-        ) == PackageManager.PERMISSION_GRANTED
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun getOutputDirectory(): File {
         val mediaDir = externalMediaDirs.firstOrNull()?.let {
             File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
         }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
+
+        return if (mediaDir != null && mediaDir.exists()) {
+            mediaDir
+        } else {
+            filesDir
+        }
     }
 
     override fun onDestroy() {
@@ -146,6 +159,7 @@ class CameraActivity : AppCompatActivity() {
     companion object {
         private const val FILENAME_FORMAT = "yyyyMMdd-HHmmssSSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
+        
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private val TAG = CameraActivity::class.simpleName
     }
